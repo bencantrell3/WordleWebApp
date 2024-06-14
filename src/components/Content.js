d7bcc9329175ed14763b72a32bb0c3ff5b19ed73
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-06-14 18:20:28 -0500

    Initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,19 @@
+import "./Content.css"
+
+function Content() {
+    return(
+        <>
+        <background></background>
+        <title>
+            <h1>
+                BEN'S WORDLE ARCHIVE
+            </h1>
+        </title>
+        <panel/>
+        <panel top="30%" right="70px" />
+        <panel2/> 
+        </>
+    );
+}
+
+export default Content;
