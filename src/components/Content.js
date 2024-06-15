9c48dea49634414b7304e9bfd2bdf051b8dc1407
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-06-14 19:52:23 -0500

    five boxes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,19 @@
-import "./Content.css"
+import "./Content.css";
 
 function Content() {
-    return(
-        <>
-        <background></background>
-        <title>
-            <h1>
-                BEN'S WORDLE ARCHIVE
-            </h1>
-        </title>
-        <panel/>
-        <panel top="30%" right="70px" />
-        <panel2/> 
-        </>
-    );
+  return (
+    <>
+      <background></background>
+      <title>
+        <h1>BEN'S WORDLE ARCHIVE</h1>
+      </title>
+      <square1></square1>
+      <square2></square2>
+      <square3></square3>
+      <square4></square4>
+      <square5></square5>
+    </>
+  );
 }
 
 export default Content;
