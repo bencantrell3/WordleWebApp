f99e6d6a9d9c368168f2082b8bfe63d777d4d85e
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-06-17 00:24:04 -0500

    started method for making squares

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,15 +5,38 @@ function Content() {
     <>
       <background></background>
       <title>
-        <h1>BEN'S WORDLE ARCHIVE</h1>
+        <h1>WORDLE ARCHIVE</h1>
       </title>
-      <square1></square1>
+      <square1/>
       <square2></square2>
       <square3></square3>
       <square4></square4>
       <square5></square5>
+      {Square1(0,100)}
     </>
-  );
+  )
 }
+
+
+const Square1 = ({ top, offset }) => {//need to get it to put arguments into the style sheet. Wierd interaction is JS and css.
+  const style = {
+    width: '70px',            
+    height: '70px' ,         
+    backgroundColor: 'rgb(0, 255, 4)',
+    position:'fixed',
+    top: '30%',              
+    right: 'calc(50% + 100px))',       
+    transform: 'translateY(-30%)',
+    border: '4px solid rgb(23, 23, 23)',
+    padding: '10px',
+    margin: '20px',
+  };
+  console.log("accessed");
+  return(
+    <div style={style}>square</div>
+  )
+}
+
+
 
 export default Content;
