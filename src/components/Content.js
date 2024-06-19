bd92cf17633569deba752219579f75bbe1787039
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-06-18 21:26:30 -0500

    working for loop

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,47 +3,42 @@ import "./Content.css";
 
 let squareArray = [];
 function Content() {
-  let x = 30;
-  let y = 130;
-  let topVar = '' + x + '%';
-  let offsetVar = 'calc(50% + ' + y + 'px)';
+  generateBoard();
   return (
     <>
       <background></background>
       <title>
         <h1>WORDLE ARCHIVE</h1>
       </title>
-      <square1/>
-      <square2></square2>
-      <square3></square3>
-      <square4></square4>
-      <square5></square5>
-      <Square1 top= {topVar} offset= {offsetVar} />
+
+      {squareArray}
     </>
   )
 }
 
 function generateBoard(){
-  for(let i = 0; i < 2; i++){
-    squareArray.push(Square())
+  for(let j = 0; j < 5; j++){
+    squareArray.push(Square(25+(i*12), 130-(j*100)))
+    console.log(j*50);
   }
 }
 
 
-const Square1 = ({ top, offset }) => {//need to get it to put arguments into the style sheet. Wierd interaction is JS and css.
-  const style = {
+let Square = (top, offset) => {//need to get it to put arguments into the style sheet. Wierd interaction is JS and css.
+  let topVar = '' + top + '%';
+  let offsetVar = 'calc(50% + ' + offset + 'px)';
+  let style = {
     width: '70px',            
     height: '70px' ,         
     backgroundColor: 'rgb(0, 255, 4)',
     position:'fixed',
-    top: top,              
-    right: offset,       
+    top: topVar,              
+    right: offsetVar,       
     transform: 'translateY(-50%)',
     border: '4px solid rgb(23, 23, 23)',
     padding: '10px',
     margin: '20px',
   };
-  console.log("accessed");
   return(
     <div style={style}>square</div>
   )
