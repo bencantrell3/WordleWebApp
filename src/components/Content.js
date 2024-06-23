62641156b5203e8ef69df67e5b56d996157c812f
Ben Cantrell <ben.cantrell@okstate.edu>
2024-06-22 23:31:08 -0500

    dimension reconfig (vh based)

--- src/components/Content.js
+++ src/components/Content.js
@@ -23,39 +23,39 @@ function Content() {
 function generateBoard(){
   for(let i = 0; i < 6; i++){
     for(let j = 0; j < 5; j++){
-      squareArray.push(Square(17+(i*8), 95-(j*82)))
+      squareArray.push(Square(14+(i*9), 10-(j*9)))
     }
   }
 }
 
 function generateQwerty(){
   for(let i = 0; i < 10; i++){
-    qwertyArray.push(qwerty(68, 232-(i*65),qwertyList[i],30))
+    qwertyArray.push(qwerty(71, 28.9-(i*8),qwertyList[i],30))
   }
   for(let i = 0; i < 9; i++){
-    qwertyArray.push(qwerty(77, 200-(i*65),qwertyList[i+10],30))
+    qwertyArray.push(qwerty(80, 24.8-(i*8),qwertyList[i+10],30))
   }
   for(let i = 0; i < 9; i++){
     if(i === 0 || i === 8)
-      qwertyArray.push(qwerty(86, 200-(i*65),qwertyList[i+19],15))
+      qwertyArray.push(qwerty(89, 24.8-(i*8),qwertyList[i+19],15))
     else
-      qwertyArray.push(qwerty(86, 200-(i*65),qwertyList[i+19],30))
+      qwertyArray.push(qwerty(89, 24.8-(i*8),qwertyList[i+19],30))
   }
 }
 
 let qwerty = (top, offset, text, size) => {
-  let topVar = '' + top + '%';
-  let offsetVar = 'calc(50% + ' + offset + 'px)';
+  let topVar = '' + top + 'vh';
+  let offsetVar = 'calc(50% + ' + offset + 'vh)';
   let style = {
-    width: '30px',            
-    height: '40px' ,         
+    width: '3vh',            
+    height: '4vh' ,         
     backgroundColor: 'rgb(30, 30, 30)',
     color: 'rgb(255,255,255)',
     position:'fixed',
     top: topVar,              
     right: offsetVar, 
     transform: 'translateY(-50%)',
-    border: '4px solid rgb(255, 255, 255)',
+    border: '0.5vh solid rgb(255, 255, 255)',
     padding: '10px',
     margin: '20px',
     borderRadius: '10px',
@@ -71,18 +71,18 @@ let qwerty = (top, offset, text, size) => {
 
 
 let Square = (top, offset) => {//need to get it to put arguments into the style sheet. Wierd interaction is JS and css.
-  let topVar = '' + top + '%';
-  let offsetVar = 'calc(50% + ' + offset + 'px)';
+  let topVar = '' + top + 'vh';
+  let offsetVar = 'calc(50% + ' + offset + 'vh)';
   let style = {
-    width: '50px',            
-    height: '50px' ,         
+    width: '6vh',            
+    height: '6vh' ,         
     backgroundColor: 'rgb(44, 44, 44)',
     position:'fixed',
     top: topVar,              
     right: offsetVar,       
     transform: 'translateY(-50%)',
     border: '4px solid rgb(23, 23, 23)',
-    padding: '10px',
+    padding: '5px',
     margin: '20px',
   };
   return(
