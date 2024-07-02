fbaf5624e33a14b8d3066f165346a8bbf5cef688
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-07-01 22:21:48 -0500

    individual squares, broken state

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,43 +5,22 @@ let squareArray = [];
 let qwertyArray = [];
 let currentGuess = [];
 let keyAdded = false;
-let qwertyList = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","ENTER","Z","X","C","V","B","N","M","BACK"];
-let statesArray = [];
-
-let stateTemplate = {
-    width: '6vh',            
-    height: '6vh' ,         
-    backgroundColor: 'rgb(255, 255, 255)',
-    position:'fixed',       
-    transform: 'translateY(-50%)',
-    border: '4px solid rgb(23, 23, 23)',
-    padding: '5px',
-    margin: '20px',
-};
-
-
-for (let i = 0; i < 30; i++) {
-  let state = {
-    ...stateTemplate,
-  };
-  statesArray.push(state);
-}
-
+let qwertyList = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","ENTER","Z","X","C","V","B","N","M","BACK"]
 function Content() {
   if(!keyAdded)
     window.addEventListener('keydown', handleKeyPress);
   keyAdded = true;
-  
-  let [board, setBoard] = useState(stateTemplate);
+  let [currentIndex, setCurrentIndex] = useState(0);
 
+  /*
   function generateBoard(){
     for(let i = 0; i < 5; i++){//only generates 5
       for(let j = 0; j < 5; j++){
-        squareArray.push(Square(14+(i*9), 10-(j*9),board[j],statesArray[i+j]));
+        squareArray.push(Square(14+(i*9), 10-(j*9),board[j]))
       }
     }
   }
-
+*/
   
   function generateQwerty(){
     for(let i = 0; i < 10; i++){
@@ -85,25 +64,24 @@ function Content() {
   }
   
   
-  let Square = (top, offset, letter,instance,state) => {//need to get it to put arguments into the style sheet. Wierd interaction is JS and css.
+  let Square = (top, offset, letter) => {//need to get it to put arguments into the style sheet. Wierd interaction is JS and css.
     let topVar = '' + top + 'vh';
     let offsetVar = 'calc(50% + ' + offset + 'vh)';
-    let currState = {
+    let style = {
       width: '6vh',            
       height: '6vh' ,         
-      backgroundColor: 'rgb(255, 255, 255)',
-      position:'fixed',       
+      backgroundColor: 'rgb(44, 44, 44)',
+      position:'fixed',
+      top: topVar,              
+      right: offsetVar,       
       transform: 'translateY(-50%)',
       border: '4px solid rgb(23, 23, 23)',
       padding: '5px',
       margin: '20px',
-      top: topVar,
-      right: offsetVar
-    }
-
-
+      
+    };
     return(
-      <div style={currState}>{letter}</div>
+      <div style={style}>{letter}</div>
     )
   }
   
@@ -122,18 +100,17 @@ function Content() {
   
   function handleKeyPress(event){
     console.log(event.key);
-    let newGuess = [...currentGuess, event.key];
-    console.log(newGuess);
-    currentGuess = newGuess;
-
-    setBoard(stateTemplate); // Update the board with the current guess
+    //let newGuess = [...currentGuess, event.key];
+    //console.log(newGuess);
+    //currentGuess = newGuess;
+    setCurrentIndex(currentIndex + 1); 
   };
 
 
-  let lastRow = [Square(14+(45), 10-(0),board[0]),Square(14+(45), 10-(9),board[1]),Square(14+(45), 10-(18),board[2]),Square(14+(45), 10-(27),board[3]),Square(14+(45), 10-(36),board[4])]
+  //let lastRow = [Square(14+(45), 10-(0),board[0]),Square(14+(45), 10-(9),board[1]),Square(14+(45), 10-(18),board[2]),Square(14+(45), 10-(27),board[3]),Square(14+(45), 10-(36),board[4])]
 
 
-  generateBoard();
+  //generateBoard();
   generateQwerty();
   
   return (
@@ -142,10 +119,44 @@ function Content() {
       <title>
         <h1>WORDLE ARCHIVE</h1>
       </title>
-      {squareArray}
       {qwertyArray}
-      {lastRow}
       {button()}
+
+      {Square(14,10,currentIndex)}
+      {Square(14,1,currentIndex)}
+      {Square(14,-8,currentIndex)}
+      {Square(14,-17,currentIndex)}
+      {Square(14,-26,currentIndex)}
+
+      {Square(23,10,currentIndex)}
+      {Square(23,1,currentIndex)}
+      {Square(23,-8,currentIndex)}
+      {Square(23,-17,currentIndex)}
+      {Square(23,-26,currentIndex)}
+      
+      {Square(32,10,currentIndex)}
+      {Square(32,1,currentIndex)}
+      {Square(32,-8,currentIndex)}
+      {Square(32,-17,currentIndex)}
+      {Square(32,-26,currentIndex)}
+
+      {Square(41,10,currentIndex)}
+      {Square(41,1,currentIndex)}
+      {Square(41,-8,currentIndex)}
+      {Square(41,-17,currentIndex)}
+      {Square(41,-26,currentIndex)}
+
+      {Square(50,10,currentIndex)}
+      {Square(50,1,currentIndex)}
+      {Square(50,-8,currentIndex)}
+      {Square(50,-17,currentIndex)}
+      {Square(50,-26,currentIndex)}
+
+      {Square(59,10,currentIndex)}
+      {Square(59,1,currentIndex)}
+      {Square(59,-8,currentIndex)}
+      {Square(59,-17,currentIndex)}
+      {Square(59,-26,currentIndex)}
     </>
   )
 }
