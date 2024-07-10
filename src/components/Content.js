25d28bbb661f18f23112c257d490bbd5c723c027
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-07-09 22:19:45 -0500

    Blurring works, new colors

--- src/components/Content.js
+++ src/components/Content.js
@@ -80,7 +80,6 @@ let colorArr = [[],[],[],[],[],[]];
 
 let index = 0;
 
-let qwertyArray = [];
 let currentGuess = [];
 let keyAdded = false;
 let qwertyList = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","ENTER","Z","X","C","V","B","N","M","BACK"]
@@ -91,25 +90,16 @@ function Content() {
   let [currentWord, setCurrentWord] = useState([]);
   let [colors, setColors] = useState(['rgb(44, 44, 44)','rgb(44, 44, 44)','rgb(44, 44, 44)','rgb(44, 44, 44)','rgb(44, 44, 44)']);
 
-  function generateQwerty(){
-    for(let i = 0; i < 10; i++){
-      qwertyArray.push(qwerty(71, 28.9-(i*8),qwertyList[i],30))
-    }
-    for(let i = 0; i < 9; i++){
-      qwertyArray.push(qwerty(80, 24.8-(i*8),qwertyList[i+10],30))
-    }
-    for(let i = 0; i < 9; i++){
-      if(i === 0 || i === 8)
-        qwertyArray.push(qwerty(89, 24.8-(i*8),qwertyList[i+19],15))
-      else
-        qwertyArray.push(qwerty(89, 24.8-(i*8),qwertyList[i+19],30))
-    }
-  }
    
   let qwerty = (top, offset, text, size) => {
+    let blurVar = '';
+    if(globalBlur){
+      blurVar = 'blur(5px)';
+    }
     let topVar = '' + top + 'vh';
     let offsetVar = 'calc(50% + ' + offset + 'vh)';
     let style = {
+      filter: blurVar,
       width: '3vh',            
       height: '4vh' ,         
       backgroundColor: 'rgb(30, 30, 30)',
@@ -282,44 +272,44 @@ function Content() {
       let temp = answer;
       //green logic
       if(currentGuess[0] == answer[0]){
-        retArray[0] = 'rgb(0, 255, 0)';
+        retArray[0] = 'rgb(43, 166, 55)';
         temp = temp.substring(1);
       }
       if(currentGuess[1] == answer[1]){
-        retArray[1] = 'rgb(0, 255, 0)';
+        retArray[1] = 'rgb(43, 166, 55)';
         temp = temp.substring(0,temp.indexOf(answer[1])) + temp.substring(temp.indexOf(answer[1])+1,temp.length);
       }
       if(currentGuess[2] == answer[2]){
-        retArray[2] = 'rgb(0, 255, 0)';
+        retArray[2] = 'rgb(43, 166, 55)';
         temp = temp.substring(0,temp.indexOf(answer[2])) + temp.substring(temp.indexOf(answer[2])+1,temp.length);
       }
       if(currentGuess[3] == answer[3]){
-        retArray[3] = 'rgb(0, 255, 0)';
+        retArray[3] = 'rgb(43, 166, 55)';
         temp = temp.substring(0,temp.indexOf(answer[3])) + temp.substring(temp.indexOf(answer[3])+1,temp.length);
       }
       if(currentGuess[4] == answer[4]){
-        retArray[4] = 'rgb(0, 255, 0)';
+        retArray[4] = 'rgb(43, 166, 55)';
         temp = temp.substring(0,temp.indexOf(answer[4])) + temp.substring(temp.indexOf(answer[4])+1,temp.length);
-      }
+      } 
       //yellow logic
       if(temp.indexOf(currentGuess[0]) != -1){
-        retArray[0] = 'rgb(255, 125, 225)';
+        retArray[0] = 'rgb(201, 188, 40)';
         temp = temp.substring(0,temp.indexOf(currentGuess[0])) + temp.substring(temp.indexOf(currentGuess[0])+1,temp.length);
       }
       if(temp.indexOf(currentGuess[1]) != -1){
-        retArray[1] = 'rgb(255, 125, 255)';
+        retArray[1] = 'rgb(201, 188, 40)';
         temp = temp.substring(0,temp.indexOf(currentGuess[1])) + temp.substring(temp.indexOf(currentGuess[1])+1,temp.length);
       }
       if(temp.indexOf(currentGuess[2]) != -1){
-        retArray[2] = 'rgb(255, 125, 255)';
+        retArray[2] = 'rgb(201, 188, 40)';
         temp = temp.substring(0,temp.indexOf(currentGuess[2])) + temp.substring(temp.indexOf(currentGuess[2])+1,temp.length);
       }
       if(temp.indexOf(currentGuess[3]) != -1){
-        retArray[3] = 'rgb(255, 125, 255)';
+        retArray[3] = 'rgb(201, 188, 40)';
         temp = temp.substring(0,temp.indexOf(currentGuess[3])) + temp.substring(temp.indexOf(currentGuess[3])+1,temp.length);
       } 
       if(temp.indexOf(currentGuess[4]) != -1){
-        retArray[4] = 'rgb(255, 125, 255)';
+        retArray[4] = 'rgb(201, 188, 40)';
         temp = temp.substring(0,temp.indexOf(currentGuess[4])) + temp.substring(temp.indexOf(currentGuess[4])+1,temp.length);
       }
 
@@ -329,7 +319,6 @@ function Content() {
   }
 
 
-  generateQwerty();
   
   return (
     <>
@@ -338,7 +327,6 @@ function Content() {
       <title>
         <h1>WORDLE PRO</h1>
       </title>
-      {qwertyArray}
       
 
       {Square(14,10,board[0][0], colorArr[0][0])}
@@ -377,6 +365,37 @@ function Content() {
       {Square(59,-17,board[5][3], colorArr[5][3])}
       {Square(59,-26,board[5][4], colorArr[5][4])}
 
+      {(qwerty(71, 28.9, qwertyList[0],30))}
+      {(qwerty(71, 20.9, qwertyList[1],30))}
+      {(qwerty(71, 12.9, qwertyList[2],30))}
+      {(qwerty(71, 4.9, qwertyList[3],30))}
+      {(qwerty(71, -3.1, qwertyList[4],30))}
+      {(qwerty(71, -11.1, qwertyList[5],30))}
+      {(qwerty(71, -19.1, qwertyList[6],30))}
+      {(qwerty(71, -27.1, qwertyList[7],30))}
+      {(qwerty(71, -35.1, qwertyList[8],30))}
+      {(qwerty(71, -43.1, qwertyList[9],30))}
+
+      {(qwerty(80, 24.8, qwertyList[10],30))}
+      {(qwerty(80, 16.8, qwertyList[11],30))}
+      {(qwerty(80, 8.8, qwertyList[12],30))}
+      {(qwerty(80, 0.8, qwertyList[13],30))}
+      {(qwerty(80, -7.2, qwertyList[14],30))}
+      {(qwerty(80, -15.2, qwertyList[15],30))}
+      {(qwerty(80, -23.2, qwertyList[16],30))}
+      {(qwerty(80, -31.2, qwertyList[17],30))}
+      {(qwerty(80, -39.2, qwertyList[18],30))}
+
+      {(qwerty(89, 24.8, qwertyList[19],15))}
+      {(qwerty(89, 16.8, qwertyList[20],30))}
+      {(qwerty(89, 8.8, qwertyList[21],30))}
+      {(qwerty(89, 0.8, qwertyList[22],30))}
+      {(qwerty(89, -7.2, qwertyList[23],30))}
+      {(qwerty(89, -15.2, qwertyList[24],30))}
+      {(qwerty(89, -23.2, qwertyList[25],30))}
+      {(qwerty(89, -31.2, qwertyList[26],30))}
+      {(qwerty(89, -39.2, qwertyList[27],15))}
+
       {Sidebar()}
 
       {CalendarComponent()}
@@ -384,6 +403,21 @@ function Content() {
     </>
   )
 }
-
+/*
+function generateQwerty(){
+    for(let i = 0; i < 10; i++){
+      qwertyArray.push(qwerty(71, 28.9-(i*8),qwertyList[i],30))
+    }
+    for(let i = 0; i < 9; i++){
+      qwertyArray.push(qwerty(80, 24.8-(i*8),qwertyList[i+10],30))
+    }
+    for(let i = 0; i < 9; i++){
+      if(i === 0 || i === 8)
+        qwertyArray.push(qwerty(89, 24.8-(i*8),qwertyList[i+19],15))
+      else
+        qwertyArray.push(qwerty(89, 24.8-(i*8),qwertyList[i+19],30))
+    }
+  }
+*/
 
 export default Content;
